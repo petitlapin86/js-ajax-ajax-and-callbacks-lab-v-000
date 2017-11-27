6c6f7c7817523a277217d2c214450fbd0a3a0fee
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-27 20:48:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var renderSearchResult = (result) => {
   return `
       <div>
         <h2><a href="${result.html_url}">${result.name}</a></h2>
-        <p><a href="#"data-repository="${result.name}"data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p><a href="#"data-repository="${result.name}"data-owner="${result.owner.login}"onclick="showCommits(this)">Show Commits</a></p>
         <p>${result.description}</p>
       </div>
       <hr>
