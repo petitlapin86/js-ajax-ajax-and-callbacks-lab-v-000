7ee6469d3613315caeb8138a5b8f75c8411e017c
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-27 20:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,27 +18,26 @@ var showCommits = (el) => {
 }
 
 var renderSearchResult = (result) => {
-  return 
+  return `
       <div>
         <h2><a href="${result.html_url}">${result.name}</a></h2>
         <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
         <p>${result.description}</p>
       </div>
       <hr>
-
+    `
 }
 
 var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
 
 var searchRepositories = () => {
   const searchTerms = $('#searchTerms').val()
-  $.get(`/https:\
-/\/api.github.com\/search\/repositories\?q=tetris/${searchTerms}`, data => {
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
       $('#results').html(renderSearchResults(data))
     }).fail(error => {
       displayError()
     })
 }
 
-$( document).ready(function (){
+$(document).ready(function (){
 });
